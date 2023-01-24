d85ef371a6a547affef39d87089589359c1eb4f4
Luke Tutton <tuttonluke@gmail.com>
2023-01-24 10:24:10 +0000

    Adds visualisation of decision tree prediction.

--- random_forests_and_bagging.py
+++ random_forests_and_bagging.py
@@ -4,7 +4,8 @@ np.set_printoptions(suppress=True)
 np.random.seed(42)
 import matplotlib.pyplot as plt
 import sklearn.datasets
-from utils import get_classification_data, show_data, colors
+import sklearn.tree
+from utils import get_classification_data, show_data, colors, visualise_predictions
 
 # %%
 def create_bootstrapped_dataset(existing_X, existing_y, size):
@@ -17,6 +18,7 @@ def create_bootstrapped_dataset(existing_X, existing_y, size):
 
 # %%
 if __name__ == "__main__":
+    # bootstrapping
     m = 500
     X, Y = get_classification_data(m=m, variant="circles", noise=0.1, factor=0.7)
     show_data(X, Y)
@@ -25,3 +27,11 @@ if __name__ == "__main__":
     dataset_size = int(m / 5)
 
     bootstrapped_datasets = create_bootstrapped_dataset(X, Y, dataset_size)
+
+    # making predictions using a single decision tree
+    
+    # large depth (overfit) vs small depth (underfit)
+    tree = sklearn.tree.DecisionTreeClassifier(max_depth=20)
+    tree.fit(X, Y)
+    visualise_predictions(tree.predict, X, Y)
+
