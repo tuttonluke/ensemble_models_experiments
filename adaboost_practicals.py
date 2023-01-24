254b8a4c6b8f9ae59762235707a2f249e2a35112
Luke Tutton <tuttonluke@gmail.com>
2023-01-24 15:11:11 +0000

    Adds grid search hyperparameter tuning.

--- adaboost_practicals.py
+++ adaboost_practicals.py
@@ -3,7 +3,7 @@ from sklearn import metrics
 from sklearn.datasets import load_breast_cancer
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import RepeatedStratifiedKFold, cross_val_score
+from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold, cross_val_score 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
@@ -73,3 +73,15 @@ for name, model in models.items():
     results.append(scores)
     names.append(name)
     print('---->Stump tree (%s)---Accuracy( %.5f)' % (name, np.mean(scores)))
+# %% TUNE HYPERPARAMETERS WITH GRID SEARCH
+# define classifier
+model = AdaBoostClassifier()
+
+grid = {}
+grid['n_estimators'] = [10, 50, 100, 200, 500]
+grid['learning_rate'] = [0.0001, 0.01, 0.1, 1.0, 1.1, 1.2]
+grid['depth'] = [i + 1 for i in range(8)]
+
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3)
+
+grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=1, cv=cv, scoring="accuracy")
