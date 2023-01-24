83dab7a5919fa3076df4ccffed10f156138597a1
Luke Tutton <tuttonluke@gmail.com>
2023-01-24 11:48:00 +0000

    Adds adaboost practicals file.

---
+++ adaboost_practicals.py
@@ -0,0 +1,11 @@
+# %%
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import numpy as np
+
+# %%
